2a6aedaa1a059172a9f64098126398af6a89b695
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-10-18 21:09:19 +0000

    Adding Form for creating articles.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "/articles", to: "articles#index"
+  get "/articles/new", to: "articles#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
