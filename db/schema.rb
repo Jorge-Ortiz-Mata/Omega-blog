fac38e539c3b0489ec8c39782ce06c85a200f736
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-10-24 17:18:25 +0000

    Rollback migration.

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_24_170901) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
-    t.boolean "is_confirmed?"
+    t.boolean "is_confirmed?", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
