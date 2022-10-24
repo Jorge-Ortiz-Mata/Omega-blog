b5f53bbdb76fbdae31e79912152f0d001b1494ed
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-10-24 20:03:00 +0000

    C for users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,21 @@ class UsersController < ApplicationController
   def new
 
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Sign up successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:session).permit(:email, :password, :password_confirmation)
+  end
 end
