0334ba44693fa058ba11454bf79bb5ef64f71f44
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-10-28 23:58:49 +0000

    Adding Token within the email body.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
 class UserMailer < ApplicationMailer
 
   def confirm_account
-    @user = 'Jorge Ortiz'
-    mail(to: 'yorch.academy@gmail.com', subject: 'Welcome to My Omega Blog')
+    @user = params[:user]
+    @url = "http://127.0.0.1:3001/#{@user.token}"
+    mail(to: @user.email, subject: 'Welcome to My Omega Blog')
   end
 end
