c3d41fa5d86d0f184d11d1dc605d86739fefa62d
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-11-01 18:25:54 +0000

    Adding Active Storage.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:name, :description)
+    params.require(:article).permit(:name, :description, :avatar)
   end
 
   def set_article
