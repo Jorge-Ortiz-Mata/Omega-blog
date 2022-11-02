bcfb7d54bf0c43ae2ab4d2cbdb8c224a2cb4d2da
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-11-02 17:06:35 +0000

    Has and belongs to many association.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,6 @@ class Article < ApplicationRecord
   validates :description, presence: true, length: { minimum: 10 }
   has_one_attached :avatar
   has_rich_text :body
+  has_and_belongs_to_many :categories
   belongs_to :user
 end
