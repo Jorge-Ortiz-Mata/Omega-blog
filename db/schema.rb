18118e84c815346c18b6e96630b760b0fdcab4ce
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-11-02 16:55:50 +0000

    CRUD for Categories.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_183015) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_165117) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "btree_gist"
@@ -82,6 +82,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_183015) do
     t.bigint "user_id"
     t.text "body"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
